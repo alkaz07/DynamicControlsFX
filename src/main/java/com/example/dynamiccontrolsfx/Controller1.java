778072806877e444e954507aa351d42ef605a4a6
alkaz07 <alkaz07@yandex.ru>
2023-04-20 19:28:05 +0300

    small refactoring

--- src/main/java/com/example/dynamiccontrolsfx/Controller1.java
+++ src/main/java/com/example/dynamiccontrolsfx/Controller1.java
@@ -3,16 +3,32 @@ package com.example.dynamiccontrolsfx;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.util.ArrayList;
+
 public class Controller1 {
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
-        int amount = 10;
+        ArrayList<Reader> readers = generateRandomReaders(10);
+        outputReaders(readers);
+    }
+
+    public void outputReaders(ArrayList<Reader> readers)
+    {
+        for (Reader r : readers  ) {
+            System.out.println(r);
+        }
+    }
+    public ArrayList<Reader> generateRandomReaders(int amount)
+    {
+        ArrayList<Reader> readers = new ArrayList<>();
         for (int i = 0; i < amount; i++) {
             Reader reader = new Reader();
-            System.out.println(reader);
+           // System.out.println(reader);
+            readers.add(reader);
         }
+        return readers;
     }
 }
