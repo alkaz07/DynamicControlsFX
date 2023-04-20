5b8d022ac1f7ec8d010aff1ab2c6f7c9dc7c950a
alkaz07 <alkaz07@yandex.ru>
2023-04-20 19:48:00 +0300

    +dynamic buttons and labels

--- src/main/java/com/example/dynamiccontrolsfx/Controller1.java
+++ src/main/java/com/example/dynamiccontrolsfx/Controller1.java
@@ -1,24 +1,33 @@
 package com.example.dynamiccontrolsfx;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 
 import java.util.ArrayList;
 
 public class Controller1 {
     @FXML
     private Label welcomeText;
+    @FXML
+    VBox boxReaders;
 
     @FXML
     protected void onHelloButtonClick() {
-        ArrayList<Reader> readers = generateRandomReaders(10);
+        ArrayList<Reader> readers = generateRandomReaders(7);
         outputReaders(readers);
     }
 
     public void outputReaders(ArrayList<Reader> readers)
     {
+        boxReaders.getChildren().clear();
         for (Reader r : readers  ) {
-            System.out.println(r);
+            //System.out.println(r);
+            Label rlab = new Label(r.toString());
+            Button rbut = new Button("ажми на "+r.fio);
+            boxReaders.getChildren().add(rlab);
+            boxReaders.getChildren().add(rbut);
         }
     }
     public ArrayList<Reader> generateRandomReaders(int amount)
--- src/main/java/com/example/dynamiccontrolsfx/MyApplication.java
+++ src/main/java/com/example/dynamiccontrolsfx/MyApplication.java
@@ -11,7 +11,7 @@ public class MyApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MyApplication.class.getResource("view_readers.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
